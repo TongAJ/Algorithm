504dd522a5ffce23da12d929d34b308b7b6d9c17
TongAJ <875334300@qq.com>
2020-09-14 15:28:45 +0800

    八皇后注释

--- src/main/java/com/self/algorithm/recursion/MyEightQueen.java
+++ src/main/java/com/self/algorithm/recursion/MyEightQueen.java
@@ -11,13 +11,19 @@ import java.util.Arrays;
  */
 public class MyEightQueen {
 
-    /** 棋盘大小 */
+    /**
+     * 棋盘大小
+     */
     private static final int MAX_LENGTH = 8;
 
-    /** 创建最后皇后所在位置稀疏数组 */
-    private static int[] queensArr = new int[MAX_LENGTH];
+    /**
+     * 创建最后皇后所在位置稀疏数组
+     */
+    private static final int[] queensArr = new int[MAX_LENGTH];
 
-    /** 记录放置了多少皇后 */
+    /**
+     * 记录放置了多少皇后
+     */
     private static int count = 0;
 
     public static void main(String[] args) {
@@ -32,10 +38,10 @@ public class MyEightQueen {
      * @return 返回是否冲突
      */
     public static boolean isConflict(int x, int[] queensArr) {
-        for (int i = 0; i < x ; i++) {
+        for (int i = 0; i < x; i++) {
             // 是否在同一行，即数组中的值是否相同
             // 是否在同一斜线上，即横坐标差值和纵坐标插值1：1，即相等
-            if (queensArr[i] == queensArr[x] || Math.abs(x-i)==Math.abs(queensArr[x]-queensArr[i])) {
+            if (queensArr[i] == queensArr[x] || Math.abs(x - i) == Math.abs(queensArr[x] - queensArr[i])) {
                 // 冲突
                 return true;
             }
@@ -46,22 +52,21 @@ public class MyEightQueen {
 
     /**
      * 放置皇后
+     *
      * @param spot 第几个皇后
      */
     public static void setQueens(int spot) {
         // 如果已经放了8个，则不继续执行
-        if(count>MAX_LENGTH){
-            return;
-        }else{
+        if (count <= MAX_LENGTH) {
             for (int i = 0; i < MAX_LENGTH; i++) {
                 // 第几个皇后放在第i个位置
                 queensArr[spot] = i;
                 // 放置之后进行冲突验证
-                if(!isConflict(spot,queensArr)){
+                if (!isConflict(spot, queensArr)) {
                     // 不冲突，则count++
                     count++;
                     // 放置下一个
-                    setQueens(spot+1);
+                    setQueens(spot + 1);
                 }
             }
         }
