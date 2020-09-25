f75d9c819e2f79f9b0e8ae505c9fc9b7b22266a4
TongAJ <875334300@qq.com>
2020-09-25 14:58:21 +0800

    插值查找算法： 使用场景：数组有序、比较均匀分布 中间值根据 查找值在整个数组中大概位置： left+（right-left)*(findValue-arr[left])/(arr[right]-arr[left])

---
+++ src/main/java/com/atguigu/algorithm/search/FibonacciSearch.java
@@ -0,0 +1,15 @@
+package com.atguigu.algorithm.search;
+
+/**
+ * 描述:
+ * 斐波那契查询
+ *
+ * @author tongaijie-9697
+ * 2020-09-16 15:58
+ */
+public class FibonacciSearch {
+
+    public static void main(String[] args) {
+
+    }
+}
