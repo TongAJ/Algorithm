45485525208a574d57afa1a3c8d322515db5f2ba
TongAJ <875334300@qq.com>
2020-11-03 13:35:21 +0800

    二叉树的前序中序和后序搜索

--- src/main/java/com/atguigu/algorithm/tree/HeroNodeDemo.java
+++ src/main/java/com/atguigu/algorithm/tree/HeroNodeDemo.java
@@ -1,5 +1,8 @@
 package com.atguigu.algorithm.tree;
 
+import lombok.Data;
+import lombok.ToString;
+
 /**
  * Description: 模拟二叉树
  *
@@ -26,6 +29,19 @@ public class HeroNodeDemo {
         heroNodeTree.prefix();
         heroNodeTree.mid();
         heroNodeTree.suffix();
+        System.out.println();
+
+        System.out.println("开始前序查找");
+        System.out.println(heroNodeTree.prefixSearch(5));
+        System.out.println();
+
+        System.out.println("开始中序查找");
+        System.out.println(heroNodeTree.midSearch(5));
+        System.out.println();
+
+        System.out.println("开始后序查找");
+        System.out.println(heroNodeTree.suffixSearch(5));
+        System.out.println();
 
 
     }
@@ -35,155 +51,185 @@ public class HeroNodeDemo {
 /**
  * 树节点
  */
-class HeroNode{
+@Data
+@ToString
+class HeroNode {
 
     private Integer id;
     private String name;
-    private HeroNode left ;
-    private HeroNode right ;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public HeroNode getLeft() {
-        return left;
-    }
-
-    public void setLeft(HeroNode left) {
-        this.left = left;
-    }
-
-    public HeroNode getRight() {
-        return right;
-    }
-
-    public void setRight(HeroNode right) {
-        this.right = right;
-    }
-
-    @Override
-    public String toString() {
-        return "HeroNode{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
+    @ToString.Exclude
+    private HeroNode left;
+    @ToString.Exclude
+    private HeroNode right;
 
     public HeroNode(Integer id, String name) {
         this.id = id;
         this.name = name;
     }
-    
+
     // 三种遍历方式，前中后就是根节点的打印顺序
 
     /**
-    * Description: 前序，先输出根节点，然后左子树，最后右子树
-    * Param:
-    * return: void
-    * Author: tongaijie
-    * Date: 2020/11/2
-    */
-    public void prefix(){
+     * Description: 前序，先输出根节点，然后左子树，最后右子树
+     * Param:
+     * return: void
+     * Author: tongaijie
+     * Date: 2020/11/2
+     */
+    public void prefix() {
         System.out.println(this);
-        if(this.getLeft() != null){
+        if (this.getLeft() != null) {
             this.getLeft().prefix();
         }
-        if(this.getRight() != null){
+        if (this.getRight() != null) {
             this.getRight().prefix();
         }
     }
 
     /**
-    * Description: 中序，先输出左子树，然后根节点，最后右子树
-    * Param:
-    * return: void
-    * Author: tongaijie
-    * Date: 2020/11/2
-    */
-    public void mid(){
-        if(this.getLeft() != null){
+     * Description: 中序，先输出左子树，然后根节点，最后右子树
+     * Param:
+     * return: void
+     * Author: tongaijie
+     * Date: 2020/11/2
+     */
+    public void mid() {
+        if (this.getLeft() != null) {
             this.getLeft().mid();
         }
         System.out.println(this);
-        if(this.getRight() != null){
+        if (this.getRight() != null) {
             this.getRight().mid();
         }
     }
 
     /**
-    * Description: 后序遍历，先左子树，然后右子树，最后根节点
-    * Param: [heroNode]
-    * return: void
-    * Author: tongaijie
-    * Date: 2020/11/2
-    */
-    public void suffix(){
-        if(this.getLeft() != null){
+     * Description: 后序遍历，先左子树，然后右子树，最后根节点
+     * Param: [heroNode]
+     * return: void
+     * Author: tongaijie
+     * Date: 2020/11/2
+     */
+    public void suffix() {
+        if (this.getLeft() != null) {
             this.getLeft().suffix();
         }
-        if(this.getRight() != null){
+        if (this.getRight() != null) {
             this.getRight().suffix();
         }
         System.out.println(this);
     }
 
     /**
-    *
-    * Description: 根据id进行节点的前序搜索
-    * @author tongaijie
-    * Param [id]
-    * Return com.atguigu.algorithm.tree.HeroNode
-    * Date 2020/11/2
-    */    
-    public HeroNode prefixSearch(Integer id){
-        if(this.getId() == id){
+     * Description: 根据id进行节点的前序搜索
+     *
+     * @author tongaijie
+     * Param [id]
+     * Return com.atguigu.algorithm.tree.HeroNode
+     * Date 2020/11/2
+     */
+    public HeroNode prefixSearch(Integer id) {
+        // 判断当前节点是否匹配
+        if (this.getId().equals(id)) {
+            // 匹配直接返回
             return this;
-        } else {
-            if( this.getLeft() != null){
-                return  this.getLeft().prefixSearch(id);
-            } else if ( this.getRight() != null){
-                return  this.getRight().prefixSearch(id);
-            }
         }
-        return null;
+        System.out.println("当前节点：" + this + "，没有找到");
+        // 如果当前不匹配，定义一个结果变量
+        HeroNode result = null;
+        // 左子节点不为空
+        if (this.getLeft() != null) {
+            // 进行递归左侧前序搜索
+            result = this.getLeft().prefixSearch(id);
+        }
+        // 如果找到了，直接返回
+        if (result != null) {
+            return result;
+        }
+        // 如果左子树都没找到，去右子树找
+        if (this.getRight() != null) {
+            result = this.getRight().prefixSearch(id);
+        }
+        // 不管找没找到，都返回result
+        return result;
     }
 
     /**
-    *
-    * Description: 根据id进行节点的中序搜索
-    * @author tongaijie
-    * Param [id]
-    * Return com.atguigu.algorithm.tree.HeroNode
-    * Date 2020/11/2
-    */
-    public HeroNode midSearch(Integer id){
-        if(this.getId() == id){
-            return this;
-        } else {
-            if( this.getLeft() != null){
-                return  this.getLeft().midSearch(id);
-            } else if ( this.getRight() != null){
-                return  this.getRight().midSearch(id);
-            }
+     * Description: 根据id进行节点的中序搜索
+     *
+     * @author tongaijie
+     * Param [id]
+     * Return com.atguigu.algorithm.tree.HeroNode
+     * Date 2020/11/2
+     */
+    public HeroNode midSearch(Integer id) {
+        // 定义一个结果变量
+        HeroNode result = null;
+        // 左子节点不为空
+        if (this.getLeft() != null) {
+            // 进行递归左侧前序搜索
+            result = this.getLeft().midSearch(id);
         }
+        // 如果变量有值，说明找到了，直接返回
+        if (result != null) {
+            return result;
+        }
+        // 判断当前节点是否匹配
+        if (this.getId().equals(id)) {
+            // 匹配直接返回
+            return this;
+        }
+        System.out.println("当前节点：" + this + "，没有找到");
+        // 如果左子树都没找到，去右子树找
+        if (this.getRight() != null) {
+            result = this.getRight().midSearch(id);
+        }
+        // 不管找没找到，都返回result
+        return result;
+    }
+    
+    /**
+    * Description: 根据id进行后序搜索
+    * Param: [id]
+    * return: com.atguigu.algorithm.tree.HeroNode
+    * Author: tongaijie
+    * Date: 2020/11/3
+    */
+    public HeroNode suffixSearch(Integer id){
+        // 定义变量
+        HeroNode result = null;
+        // 如果左子树不为空
+        if(this.getLeft()!=null){
+            // 向左进行后序搜索
+            result = this.getLeft().suffixSearch(id);
+        }
+        // 判断变量是否为空
+        if(result!=null){
+            // 找到节点
+            return result;
+        }
+        // 如果右子树不为空
+        if(this.getRight()!=null){
+            // 向右进行后序搜索
+            result = this.getRight().suffixSearch(id);
+        }
+        // 如果变量不为空
+        if(result!=null){
+            // 找到节点
+            return result;
+        }
+        // 判断是否匹配
+        if(this.getId().equals(id)){
+            // 匹配则直接返回
+            return this;
+        }
+        System.out.println("当前节点：" + this + "，没有找到");
+        // 都没周到，返回null
         return null;
     }
 }
 
-class HeroNodeTree{
+class HeroNodeTree {
 
     private HeroNode root;
 
@@ -192,52 +238,99 @@ class HeroNodeTree{
     }
 
     /**
-    * Description: 二叉树前序遍历
-    * Param: []
-    * return: void
-    * Author: tongaijie
-    * Date: 2020/11/2
-    */
-    public void prefix(){
+     * Description: 二叉树前序遍历
+     * Param: []
+     * return: void
+     * Author: tongaijie
+     * Date: 2020/11/2
+     */
+    public void prefix() {
         System.out.println("二叉树前序遍历");
-        if(root != null){
+        if (root != null) {
             root.prefix();
-        } else{
+        } else {
             System.out.println("二叉树根节点不能为空");
         }
     }
 
     /**
-    * Description: 二叉树中序遍历
-    * Param: []
-    * return: void
-    * Author: tongaijie
-    * Date: 2020/11/2
-    */
-    public void mid(){
+     * Description: 二叉树中序遍历
+     * Param: []
+     * return: void
+     * Author: tongaijie
+     * Date: 2020/11/2
+     */
+    public void mid() {
         System.out.println("二叉树中序遍历");
-        if(root != null){
+        if (root != null) {
             root.mid();
-        } else{
+        } else {
             System.out.println("二叉树根节点不能为空");
         }
     }
 
     /**
-    * Description: 二叉树后序遍历
-    * Param: []
-    * return: void
-    * Author: tongaijie
-    * Date: 2020/11/2
-    */
-    public void suffix(){
+     * Description: 二叉树后序遍历
+     * Param: []
+     * return: void
+     * Author: tongaijie
+     * Date: 2020/11/2
+     */
+    public void suffix() {
         System.out.println("二叉树后序遍历");
-        if(root != null){
+        if (root != null) {
             root.suffix();
-        } else{
+        } else {
             System.out.println("二叉树根节点不能为空");
         }
     }
 
+    /**
+     * Description: 根据id对二叉树进行前序搜索
+     * Param: [id]
+     * return: com.atguigu.algorithm.tree.HeroNode
+     * Author: tongaijie
+     * Date: 2020/11/3
+     */
+    public HeroNode prefixSearch(Integer id) {
+        // 如果根节点为空，直接返回
+        if (root == null) {
+            System.out.println("根节点为空，无法搜索");
+            return null;
+        }
+        return root.prefixSearch(id);
+    }
+
+    /**
+     * Description: 根据id进行二叉树的中序查询
+     * Param: [id]
+     * return: com.atguigu.algorithm.tree.HeroNode
+     * Author: tongaijie
+     * Date: 2020/11/3
+     */
+    public HeroNode midSearch(Integer id) {
+        // 如果根节点为空，直接返回
+        if (root == null) {
+            System.out.println("根节点为空，无法搜索");
+            return null;
+        }
+        return root.midSearch(id);
+    }
+
+    /**
+    * Description: 根据id进行二叉树的后序搜索
+    * Param: [id]
+    * return: com.atguigu.algorithm.tree.HeroNode
+    * Author: tongaijie
+    * Date: 2020/11/3
+    */
+    public HeroNode suffixSearch(Integer id) {
+        // 如果根节点为空，直接返回
+        if (root == null) {
+            System.out.println("根节点为空，无法搜索");
+            return null;
+        }
+        return root.suffixSearch(id);
+    }
 
 }
